8e6394c359376f170ac2af98c478512a9b0c3601
Joshua Navi <jnavi@ucsd.edu>
2016-04-12 16:07:38 -0700

    Added schema for drinks and comments

--- models.js
+++ models.js
@@ -14,10 +14,24 @@ var NewsFeedSchema = new Schema({
 	"user": String,
 	"message": String,
 	"posted": Date
-})
+});
+
+var DrinkSchema = new Schema({
+  "id": String,
+  "name": String,
+  "type": String,
+  "description": String
+});
+
+var CommentSchema = new Schema({
+  "id": String,
+  "drinkID": { type: Schema.Types.ObjectID, ref: 'Drink' },
+  'userID': { type: Schema.Types.ObjectID, ref: 'User' },
+  "text" : String
+});
+
 
 exports.NewsFeed = mongoose.model('NewsFeed', NewsFeedSchema);
-
-
-
 exports.User = mongoose.model('User', UserSchema);
+exports.Drink = mongoose.model('Drink', DrinkSchema);
+exports.Comment = mongoose.model('Comment', CommentSchema);
