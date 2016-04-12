3e6df2df362afbf3dfbb0b57a0f158ec9686ae6f
Joshua Navi <jnavi@ucsd.edu>
2016-04-12 15:28:22 -0700

    Uncommented io require statement

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-// const io = require("socket.io")(http);
+const io = require("socket.io")(http);
 const path = require("path");
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
